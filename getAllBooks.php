db7d7722a90c4dc072ff5e8352b541f5f0e7488a
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2017-09-22 13:36:46 -0700

    Latest files

--- getAllBooks.php
+++ getAllBooks.php
@@ -6,6 +6,30 @@ $booksJson = file_get_contents('books.json');
 
 $books = json_decode($booksJson, true);
 
+if (isset($_GET['keyword'])) {
+	$keyword = $_GET['keyword'];
+} else {
+	$keyword = '';
+}
+
+if(isset($_GET['caseSensitive'])) {
+ 	$caseSensitive = true;
+ } else {
+ 	$caseSensitive = false;
+ }
+
+dump($caseSensitive);
+
+$keyword = $_GET['keyword'];
+
+if ($keyword != '') {
+	foreach($books as $title => $book) {
+		if ($title != $keyword) {
+			unset($books[$title]);
+		}
+	}
+}
+
 #dump($books);
 
 #dump($books['The Bell Jar']['author']);
--- index.php
+++ index.php
@@ -6,11 +6,32 @@
 
 	<title></title>
 	<meta charset='utf-8'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="books.css">
 
 </head>
 <body>
 
 <h1>Foobooks</h1>
+
+<form method='GET' action='/'>
+
+<label for='keyword'>Filter by keyword</label>
+<input type='text' name='keyword' id='keyword' value='<?=sanitize($keyword)?>'><br>
+
+
+<input type='checkbox' name='caseSensitive' id='caseSensitive'>
+<label for='caseSensitive'>Case sensitive</label><br>
+
+
+<input type='submit' class='btn btn-primary btn-small' value='Filter books'>
+</form>
+
+<?php if(count($books) == 0) : ?>
+	<div class='alert alert-warning'>Your keyword did not match any results</div>
+<?php elseif($keyword != '') : ?>
+	<div class='alert alert-info'>You searched for: <strong><?=sanitize($keyword)?></strong></div>
+<?php endif; ?>
 
 <?php foreach ($books as $title => $book) : ?>
 	<div class='book' style='border:1px solid black'>
